b000fe94a331dac515df5d6e15110dc92454c0ba
Diogo Lucas de Oliveira <dluks82@gmail.com>
2024-02-26 19:57:13 -0300

    manipular credenciais de bd em diferentes ambientes

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,16 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+  // console.log("Credenciais do Postgres:", {
+  //   host: process.env.POSTGRES_HOST,
+  //   port: process.env.POSTGRES_PORT,
+  //   user: process.env.POSTGRES_USER,
+  //   database: process.env.POSTGRES_DB,
+  //   password: process.env.POSTGRES_PASSWORD,
+  // });
 
   try {
     await client.connect();
